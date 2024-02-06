fb311768a7a0277bcc2120132d6d99cd43a5c2b3
Emily Zhao <35831739+emilydidthis@users.noreply.github.com>
2024-02-06 15:24:37 -0500

    day 05/06 class materials

---
+++ days_05_06/DOM_queries_attributes_styles_simple_events/02_SimpleEvents.js
@@ -0,0 +1,21 @@
+// define the function that will be run when the 2nd div is clicked
+function doInlineEvent() {
+  alert("The doInlineEvent function was called");
+}
+
+// get a reference to the element with the ID of 'three'
+const elThree = document.getElementById('three');
+
+// tell this element to set up an event for when the user
+// clicks on it. this method is preferred over setting up inline listeners
+elThree.onclick = function() {
+  alert("This was set up in the external JS file");
+}
+
+// get a reference to the element with the ID of 'four'
+const elFour = document.getElementById('four');
+
+// you can add multiple event listeners on the same element
+// anonymous function syntax
+elFour.addEventListener("click", ()=> alert("DOM Event Listener"))
+elFour.addEventListener("click", ()=> console.log("DOM Event Listener"))
---
+++ days_05_06/DOM_queries_attributes_styles_simple_events/03_MouseEventDemo.js
@@ -0,0 +1,6 @@
+// grab a reference to the element with id 'box' 
+
+// grab a reference to the 'history' panel
+
+// attach a series of mouse events to this element with id 'box'
+// 1. add "clicked" text to the 'history' panel when 'box' is clicked
