798ac3351a73d5b009e913d14ce56305fb3dc392
Emily Zhao <35831739+emilydidthis@users.noreply.github.com>
2024-02-13 14:52:12 -0500

    programming challenge setup

---
+++ days_07_08/02-DOM_augmentation_pointers/11_Example-PokemonReorder.js
@@ -0,0 +1,7 @@
+// try making the move right functionality first
+// once that works, you can easily implement for left
+
+// how do you access the tile the arrow is in?
+// how do you get the tile to the right?
+// how do you then move that tile over?
+// don't move the tile if it reaches the edge!
