4702ace1edc42042bae8833a86eda64cd9fb6556
Emily Zhao <35831739+emilydidthis@users.noreply.github.com>
2024-02-13 14:44:54 -0500

    prepping code challenge

---
+++ days_07_08/02-DOM_augmentation_pointers/10_Example-PokemonPowerup.js
@@ -0,0 +1,11 @@
+// get all '.tile' divs
+
+// set up a click handler on all '.tile' divs
+    // get a reference to the element with the 'power' class inside of this div
+
+    // extract the current power value
+
+    // increase power
+
+    // update power
+
