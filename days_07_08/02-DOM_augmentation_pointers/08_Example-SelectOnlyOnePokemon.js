ec3587d495f4ced34b2b2a1ef47c75c2e8554b74
Emily Zhao <35831739+emilydidthis@users.noreply.github.com>
2024-02-13 14:25:45 -0500

    updating code examples

---
+++ days_07_08/02-DOM_augmentation_pointers/08_Example-SelectOnlyOnePokemon.js
@@ -0,0 +1,7 @@
+// set up every image on the page to respond to click events
+// if the image is clicked, add the blue class to it
+
+
+// what happens when you click a second image?
+// we only want one selected image at a time
+
