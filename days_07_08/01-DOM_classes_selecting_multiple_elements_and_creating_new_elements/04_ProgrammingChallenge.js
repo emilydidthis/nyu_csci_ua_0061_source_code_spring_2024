1267d206cfb55e76655f3b12254e79a39348e1f2
Emily Zhao <35831739+emilydidthis@users.noreply.github.com>
2024-02-13 13:41:05 -0500

    day 07 and 08 code

---
+++ days_07_08/01-DOM_classes_selecting_multiple_elements_and_creating_new_elements/04_ProgrammingChallenge.js
@@ -0,0 +1,23 @@
+// grab references to each of our tabs
+const tabWelcome = document.getElementById('tabWelcome');
+const tabTraditional = document.getElementById('tabTraditional');
+const tabGlazed = document.getElementById('tabGlazed');
+
+// grab references to each content panel
+const contentWelcome = document.getElementById('contentWelcome');
+const contentTraditional = document.getElementById('contentTraditional');
+const contentGlazed = document.getElementById('contentGlazed');
+
+// set up click handlers to let the user click on each tab
+tabWelcome.onclick = function() {
+  // first, make this tab our 'active' tab by applying the 'active' class to it
+
+  // remove the 'active' class from the other tabs
+
+  // show the content associated with this tab
+
+  // hide the other two tabs
+
+}
+
+
